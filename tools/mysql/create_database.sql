ffcd21aff0dba9d3701efffc45018a2c4c779691
dl6kbg <oliver.goldenstein@gmail.com>
2013-03-08 19:01:15 +0000

    Added database creation file in tools/mysql folder.

--- tools/mysql/create_database.sql
+++ tools/mysql/create_database.sql
@@ -1,3 +1,19 @@
+CREATE DATABASE dump1090;
+
+GRANT ALL PRIVILEGES
+ON dump1090.*
+TO 'pi'@'localhost'
+IDENTIFIED BY 'raspberry'
+WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES
+ON dump1090.*
+TO 'pi'@'%'
+IDENTIFIED BY 'raspberry'
+WITH GRANT OPTION;
+
+USE dump1090;
+
 CREATE TABLE IF NOT EXISTS `tracks` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `icao` varchar(6) COLLATE utf8_unicode_ci NOT NULL,
@@ -6,5 +22,5 @@ CREATE TABLE IF NOT EXISTS `tracks` (
   `lon` decimal(17,14) NOT NULL,
   `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
