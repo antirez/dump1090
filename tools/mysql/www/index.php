e2f06cdbd0ec3a832a186ea2148ae759ad02b1ca
dl6kbg <oliver.goldenstein@gmail.com>
2013-03-20 19:14:54 +0000

    added a autorefreshing new webpage.

--- tools/mysql/www/index.php
+++ tools/mysql/www/index.php
@@ -1,56 +1,24 @@
-<head>
-                <title>dump1090 modes-s live traffic</title>
-                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<html>
+	<head>
+		<title>dump1090 live traffic</title>
+      		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
                 <meta charset="UTF-8">
                 <meta name="author" content="dl6kbg" />
                 <meta name="description" content="dump1090 live traffic" />
-                <meta http-equiv="refresh" content="1" /> 
 		<link rel="stylesheet" type="text/css" media="all" href="style.css" />
 
-</head>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
+		<script type="text/javascript">
+			var auto_refresh = setInterval(
+				function () {
+					$('#flights').load('table.php').fadeIn("slow");
+				}, 1000); // refresh every 1000 milliseconds
+		</script>
 
+	</head>
 
-<?php
+<body>
+<div id="flights"> </div>
+</body>
 
-$con = mysql_connect("127.0.0.1","pi","raspberry");
-
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-
-mysql_select_db("dump1090", $con);
-
-// this query gives us the live flight table. latest entries are on top
-
-$result = mysql_query("SELECT * FROM flights ORDER by last_update DESC");
-
-//html formatted output
-echo "<table><tr><th>Airline</th><th>Callsign</th><th>Squawk</th><th>ICAO</th><th>Country</th><th>Lat</th><th>Lon</th><th>Alt</th><th>VR</th><th>Heading</th><th>Speed</th><th>DF</th><th>Msgs</th><th>last updated</th></tr>";
-while ($row = mysql_fetch_array($result))
-        {
-
-        echo "<tr style=background-color:#" . $row['icao'] . ">";
-        echo "<td>" . $row['airline'] . "</td>";
-	echo "<td>" . $row['flight'] . "</td>";
-        echo "<td>" . $row['squawk'] . "</td>";
-        echo "<td>" . $row['icao'] . "</td>";
-	echo "<td>" . $row['country'] . "</td>";
-        echo "<td>" . $row['lat'] . "</td>";
-        echo "<td>" . $row['lon'] . "</td>";
-        echo "<td>" . $row['alt'] . "</td>";
-        echo "<td>" . $row['vr'] . "</td>";
-        echo "<td>" . $row['heading'] . "</td>";
-        echo "<td>" . $row['speed'] . "</td>";
-        echo "<td>" . $row['df'] . "</td>";
-        echo "<td>" . $row['msgs'] . "</td>";
-        echo "<td>" . $row['last_update'] . "</td>";
-	echo "</tr>";
-}
-
-
-echo "</table>";
-
-mysql_close($con);
-?>
-
+</html>
---
+++ tools/mysql/www/table.php
@@ -0,0 +1,44 @@
+<?php
+
+$con = mysql_connect("127.0.0.1","pi","raspberry");
+
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("dump1090", $con);
+
+// this query gives us the live flight table. latest entries are on top
+
+$result = mysql_query("SELECT * FROM flights ORDER by last_update DESC LIMIT 25");
+
+//html formatted output
+echo "<table><tr><th>Airline</th><th>Callsign</th><th>Squawk</th><th>ICAO</th><th>Country</th><th>Lat</th><th>Lon</th><th>Alt</th><th>VR</th><th>Heading</th><th>Speed</th><th>DF</th><th>Msgs</th><th>last updated</th></tr>";
+while ($row = mysql_fetch_array($result))
+        {
+
+        echo "<tr style=background-color:#" . $row['icao'] . ">";
+        echo "<td>" . $row['airline'] . "</td>";
+	echo "<td>" . $row['flight'] . "</td>";
+        echo "<td>" . $row['squawk'] . "</td>";
+        echo "<td>" . $row['icao'] . "</td>";
+	echo "<td>" . $row['country'] . "</td>";
+        echo "<td>" . $row['lat'] . "</td>";
+        echo "<td>" . $row['lon'] . "</td>";
+        echo "<td>" . $row['alt'] . "</td>";
+        echo "<td>" . $row['vr'] . "</td>";
+        echo "<td>" . $row['heading'] . "</td>";
+        echo "<td>" . $row['speed'] . "</td>";
+        echo "<td>" . $row['df'] . "</td>";
+        echo "<td>" . $row['msgs'] . "</td>";
+        echo "<td>" . $row['last_update'] . "</td>";
+	echo "</tr>";
+}
+
+
+echo "</table>";
+
+mysql_close($con);
+?>
+
