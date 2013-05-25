b5d00383156edda300d70e7fd428ecef16054a22
Brian Davenport <the.lost.minded@gmail.com>
2013-05-25 15:53:56 +0000

    Fixes the selection bugs

--- public_html/script.js
+++ public_html/script.js
@@ -414,5 +414,7 @@ function selectPlaneByHex(hex) {
 			Planes[SelectedPlane].funcUpdateLines();
 			Planes[SelectedPlane].marker.setIcon(Planes[SelectedPlane].funcGetIcon());
 		}
+	} else { 
+		SelectedPlane = null
 	}
 }
