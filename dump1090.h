d9de1190f0cf84e02712bb8550a5daf7a99df3d9
Malcolm Robb <Support@ATTAvionics.com>
2013-09-20 16:50:38 +0100

    Version update

--- dump1090.h
+++ dump1090.h
@@ -37,7 +37,7 @@
 // MinorVer changes when additional features are added, but not for bug fixes (range 00-99)
 // DayDate & Year changes for all changes, including for bug fixes. It represent the release date of the update
 //
-#define MODES_DUMP1090_VERSION     "1.07.1908.13"
+#define MODES_DUMP1090_VERSION     "1.07.2009.13"
 
 // ============================= Include files ==========================
 
