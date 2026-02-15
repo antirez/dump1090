3f05cfde7dc3a21f2bd1d86f3dcbb57a2330ef55
Edunj <edunogalj@gmail.com>
2026-02-15 15:43:56 +0100

    Add --ppm command line option for frequency correction.
    Moves ppm_error from a local variable hardcoded to 0 into the Modes
    global config, and adds a --ppm <error> CLI option so users can set
    the receiver's PPM error correction.
    
    Based on PR #96 by Edunj.
    
    Co-Authored-By: Edunj <edunogalj@gmail.com>

--- dump1090.c
+++ dump1090.c
@@ -150,6 +150,7 @@ struct {
     int enable_agc;
     rtlsdr_dev_t *dev;
     int freq;
+    int ppm_error;
 
     /* Networking */
     char aneterr[ANET_ERR_LEN];
@@ -314,6 +315,7 @@ void modesInitConfig(void) {
     Modes.aggressive = 0;
     Modes.interactive_rows = getTermRows();
     Modes.loop = 0;
+    Modes.ppm_error = 0;
 }
 
 void modesInit(void) {
@@ -383,7 +385,6 @@ void modesInit(void) {
 void modesInitRTLSDR(void) {
     int j;
     int device_count;
-    int ppm_error = 0;
     char vendor[256], product[256], serial[256];
 
     device_count = rtlsdr_get_device_count();
@@ -423,7 +424,7 @@ void modesInitRTLSDR(void) {
     } else {
         fprintf(stderr, "Using automatic gain control.\n");
     }
-    rtlsdr_set_freq_correction(Modes.dev, ppm_error);
+    rtlsdr_set_freq_correction(Modes.dev, Modes.ppm_error);
     if (Modes.enable_agc) rtlsdr_set_agc_mode(Modes.dev, 1);
     rtlsdr_set_center_freq(Modes.dev, Modes.freq);
     rtlsdr_set_sample_rate(Modes.dev, MODES_DEFAULT_RATE);
@@ -2791,6 +2792,7 @@ void showHelp(void) {
 "--gain <db>              Set gain (default: max gain. Use -100 for auto-gain).\n"
 "--enable-agc             Enable the Automatic Gain Control (default: off).\n"
 "--freq <hz>              Set frequency (default: 1090 Mhz).\n"
+"--ppm <error>            Set receiver error in parts per million (default: 0).\n"
 "--ifile <filename>       Read data from file (use '-' for stdin).\n"
 "--loop                   With --ifile, read the same file in a loop.\n"
 "--interactive            Interactive mode refreshing data on screen.\n"
@@ -2862,6 +2864,8 @@ int main(int argc, char **argv) {
             Modes.enable_agc++;
         } else if (!strcmp(argv[j],"--freq") && more) {
             Modes.freq = strtoll(argv[++j],NULL,10);
+        } else if (!strcmp(argv[j],"--ppm") && more) {
+            Modes.ppm_error = atoi(argv[++j]);
         } else if (!strcmp(argv[j],"--ifile") && more) {
             Modes.filename = strdup(argv[++j]);
         } else if (!strcmp(argv[j],"--loop")) {
